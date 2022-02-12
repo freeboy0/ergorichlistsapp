efc561efa80e21e7179d57603e8fff2f28941985
freeboy0 <96985241+freeboy0@users.noreply.github.com>
2022-02-12 02:45:20 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import pandas as pd
 import streamlit as st
 import os
 
-st.write("""Ergo token richlists""")
+st.write("""# Ergo token rich-lists""")
+st.write("""Last scan: Feb 12, 2022""")
+
 tokens = [i.replace('.csv','') for i in os.listdir('dataframes')]
 
 token = st.radio("Select a token:", tokens)
@@ -10,5 +12,6 @@ token = st.radio("Select a token:", tokens)
 df = pd.read_csv('dataframes/%s.csv'%token)
 df = df.drop(columns=[i for i in df.columns.values if 'unnamed' in i.lower()])
 
-st.write("Richlist for %s"%token)
+st.write("Rich-list for %s"%token)
 st.dataframe(df)
+
